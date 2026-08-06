f034b7d439f6c75919c145a50215879933f2dd76
Luis Fernando Cabañas Torres <luis.cabanas@ingenierosmafur.com>
2026-08-06 11:37:07 -0600

    Change name

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SostenibilidApp',
+      title: 'Sostenibilidapp',
       debugShowCheckedModeBanner: false,
       initialRoute: SplashScreen.routeName,
       routes: {
